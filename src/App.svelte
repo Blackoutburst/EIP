64d19b4bf1558843a20bb4d8941ca32f8c9fe1fc
Blackoutburst <blackoutburst@hotmail.com>
2021-10-21 10:36:17 +0200

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<header>
+    <div class="header">
+        <img src="img/splash.jpg" alt="splash" class="splash">
+    </div>
+
+    <div class="presentation">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem cupiditate, qui in maiores, illo eum tempora iure corporis omnis nemo alias maxime modi! Aliquid laborum dolorem magni perferendis adipisci vel.
+    </div>
+</header>
+
+<style>
+.splash {
+    width: 100%;
+    padding: 0px;
+    margin: 0px;
+}
+
+.presentation {
+    font-size: 2vw;
+    text-align: center;
+    padding-top: 10vh;
+    margin-left: 25vw;
+    margin-right: 25vw;
+}
+</style>
