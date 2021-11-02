756b6128d5ba14f5c46e4953f2462c5e61cf383f
Blackoutburst <blackoutburst@hotmail.com>
2021-11-02 08:54:02 +0100

    Added basic informations

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,36 @@
     </div>
 
     <div class="presentation">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem cupiditate, qui in maiores, illo eum tempora iure corporis omnis nemo alias maxime modi! Aliquid laborum dolorem magni perferendis adipisci vel.
+        <p>
+            Crystal Editor est un éditeur graphique capable de s'adapter à tous les moteurs de jeux du marché. <br>
+            <br>
+            Conçus avec des technologies émargeantes telle que:<br>
+            <a href="https://www.rust-lang.org" target="_blank" rel="noopener noreferrer">Rust</a>, <a href="https://tauri.studio/" target="_blank" rel="noopener noreferrer">Tauri</a> et <a href="https://svelte.dev" target="_blank" rel="noopener noreferrer">Svelte</a>, cet éditeur vous simplifiera la vie lors de vos développements de jeux vidéo.<br>
+            <br>
+            Le tout avec une possibilité de personnalisation complète afin de réellement répondre à vos besoins.<br>
+            <br>
+            Ce projet est en relation directe avec le moteur de jeu <a href="https://amethyst.rs" target="_blank" rel="noopener noreferrer">Amethyst</a>
+        </p>
+    </div>
+
+    <div class="equipe">
+        <h2>Notre équipe</h2>
+        <p>
+            yes
+        </p>
+    </div>
+
+    <div class="log">
+        <h2>Nos actions</h2>
+        <h4 style="padding-top: 5vh;">Article medium</h4>
+        <p>
+            Création d'un article medium afin de promouvoir notre projet.<br>
+            <a href="https://medium.com/@lyvia.mallereau/crystal-editor-un-éditeur-pour-engine-open-source-réalisé-par-des-étudiants-7f9a8497b630" target="_blank" rel="noopener noreferrer">Lien vers l'article</a>
+        </p>
+        <h4  style="padding-top: 5vh;">Site vitrine du projet</h4>
+        <p>
+            Le site que vous consultez actuellement ;)
+        </p>
     </div>
 </header>
 
@@ -15,11 +44,28 @@
     margin: 0px;
 }
 
+.log {
+    font-size: 1.5vw;
+    text-align: center;
+    margin-left: 20vw;
+    margin-right: 20vw;
+    padding-bottom: 10vh;
+}
+
+.equipe {
+    font-size: 1.5vw;
+    text-align: center;
+    margin-left: 25vw;
+    margin-right: 25vw;
+    padding-bottom: 10vh;
+}
+
 .presentation {
     font-size: 2vw;
     text-align: center;
     padding-top: 10vh;
     margin-left: 25vw;
     margin-right: 25vw;
+    padding-bottom: 10vh;
 }
 </style>
