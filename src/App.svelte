8a42d8d5d5037708d23925bfa1f9fb7a8a9ffae5
Blackoutburst <blackoutburst@hotmail.com>
2021-11-03 10:33:51 +0100

    typo

--- src/App.svelte
+++ src/App.svelte
@@ -19,7 +19,7 @@
     <div class="equipe">
         <h2>Notre équipe</h2>
         <p>
-            Xx_D4rk-4l3c -F3rr4r1_xX<br>
+            Xx_D4rk-4l3c-F3rr4r1_xX<br>
             Xx_D4rk-P4ul-N4rch4l_xX<br>
             Xx_D4rk-Fl0r14n-Ch4ns0n_xX<br>
             Xx_D4rk-Lyv14-M4ll3r34u_xX<br>
